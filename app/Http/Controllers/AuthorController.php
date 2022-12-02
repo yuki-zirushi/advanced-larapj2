a6f7c050cddd0a695f06ab360194884fc545138f
yuki-zirushi <yuko.12.151@gmail.com>
2022-12-02 16:36:31 +0900

    [Update]

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Author;
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+}
